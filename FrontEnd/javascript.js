d28b8ea51d73abc9acaca001dec0348a3ed1ce52
Benjamin Hautefaye <titouchef@hotmail.fr>
2023-02-28 15:00:44 +0100

    Mis en place du Repo

---
+++ FrontEnd/javascript.js
@@ -0,0 +1,77 @@
+let worksData = [];
+
+const fetchWorks = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((promise) => {
+      worksData = promise;
+    });
+};
+
+  const worksDisplay = async () => {
+  await fetchWorks();
+
+  const displayWorks = (category = "all") => {
+    const gallery = document.querySelector(".gallery");
+    const filteredWorks = worksData.filter(
+      (work) => work.category.name === category || category === "all"
+    );
+
+    gallery.innerHTML = filteredWorks
+      .map(
+        (work) =>
+          `<figure id="${work.id}" class="displayOn" data-${work.category.name}>
+            <div id="${work.categoryId}"></div>
+            <div id="${work.category.name}"></div>
+            <div id="${work.userId}"></div>
+            <img src="${work.imageUrl}" crossOrigin="anonymous"/>
+            <figcaption>${work.title}</figcaption>
+          </figure>
+        `
+      )
+      .join("");
+  };
+
+  
+  const initMenu = () => {
+    const menu = document.querySelector(".btn-wrapper");
+    menu.innerHTML = '<li><button class="filter-btn" data-category="all">Tous</button></li>';
+
+    fetch("http://localhost:5678/api/categories")
+      .then((res) => res.json())
+      .then((promise) => {
+        menu.innerHTML += promise
+          .map(
+            (category) =>
+              `<li><button class="filter-btn" data-category="${category.name}">${category.name}</button></li>
+            `
+          )
+          .join("");
+
+        const categoryButtons = document.querySelectorAll(".filter-btn");
+        categoryButtons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const category = button.dataset.category;
+            displayWorks(category);
+          });
+        });
+
+        displayWorks();
+      });
+  };
+
+  initMenu();
+};
+
+  
+
+worksDisplay();
+
+
+
+
+//
+
+
+//
+
---
+++ FrontEnd/login.js
@@ -0,0 +1,41 @@
+const form = {
+    email: document.getElementById("email"),
+    password: document.getElementById("password"),
+    submit: document.getElementById('submit'),
+    errorMessage: document.getElementById('error-message'),
+  };
+   
+  let button = form.submit.addEventListener("click", (e) => {
+    e.preventDefault();
+   const login = "http://localhost:5678/api/users/login";
+  
+    fetch(login, {
+      method: "POST",
+      headers: {
+         'Accept': 'application/json, text/plain, */*',
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: form.email.value,
+        password: form.password.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        var token = data.token;
+        var isLoggedIn = true;
+        if (data.message === "user not found") {
+          form.errorMessage.innerHTML = "Erreur dans l’identifiant ou le mot de passe";
+        } 
+        else {
+          sessionStorage.setItem("isLoggedIn", isLoggedIn);
+          sessionStorage.setItem("token", token);
+          //open("index.html");
+          location.assign("index.html"); 
+        }   
+          console.log(token, isLoggedIn);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
